1c0ae2a0f106fa878c3a7bd7d40a00c2fccf7b6d
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2019-09-24 15:38:54 +0300

    Add support for multiple Jupyter Hub installations

--- src/main/java/gr/helix/core/common/model/NotebookServerRequest.java
+++ src/main/java/gr/helix/core/common/model/NotebookServerRequest.java
@@ -12,17 +12,31 @@ public class NotebookServerRequest implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private final String      serverUrl;
+
     private final String      userName;
 
     private final String      dataDirectory;
 
     @JsonCreator
     public NotebookServerRequest(
+        @JsonProperty("serverUrl") String serverUrl,
         @JsonProperty("userName") String userName,
         @JsonProperty("dataDirectory") String dataDirectory
     ) {
+        this.serverUrl = serverUrl;
         this.userName = userName;
         this.dataDirectory = dataDirectory;
+    }
+
+    /**
+     * The Jupyter Hub URL
+     */
+    @Email
+    @NotBlank
+    @JsonProperty("serverUrl")
+    public String serverUrl() {
+        return this.serverUrl;
     }
 
     /**
