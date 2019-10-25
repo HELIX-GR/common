7c420cb925d96fd6063330b55294094ac8911372
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2019-10-25 12:01:04 +0300

    Update security configuration
    Moved shared security configuration from lab to common submodule

---
+++ src/main/java/gr/helix/core/common/service/CustomUserDetailsService.java
@@ -0,0 +1,12 @@
+package gr.helix.core.common.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import gr.helix.core.common.domain.AccountEntity;
+import gr.helix.core.common.model.security.User;
+
+public interface CustomUserDetailsService extends UserDetailsService {
+
+    public User createUser(AccountEntity a);
+
+}
