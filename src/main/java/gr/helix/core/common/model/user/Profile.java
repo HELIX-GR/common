8f9b9526080049dd80b270ff911ae8b9cb2bd1b6
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2019-10-25 15:59:15 +0300

    Add Profile class
    Copied Profile class from core to common submodule

---
+++ src/main/java/gr/helix/core/common/model/user/Profile.java
@@ -0,0 +1,42 @@
+package gr.helix.core.common.model.user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.util.Assert;
+
+public class Profile {
+
+    private Account                  account;
+
+    private List<FavoriteCollection> collections = new ArrayList<FavoriteCollection>();
+
+    private List<Favorite>           favorites   = new ArrayList<Favorite>();
+
+    public Account getAccount() {
+        return this.account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public List<FavoriteCollection> getCollections() {
+        return this.collections;
+    }
+
+    public void setCollections(List<FavoriteCollection> collections) {
+        Assert.notNull(collections, "List of FavoriteCollection must not be null");
+        this.collections = collections;
+    }
+
+    public List<Favorite> getFavorites() {
+        return this.favorites;
+    }
+
+    public void setFavorites(List<Favorite> favorites) {
+        Assert.notNull(favorites, "List of Favorite must not be null");
+        this.favorites = favorites;
+    }
+
+}
